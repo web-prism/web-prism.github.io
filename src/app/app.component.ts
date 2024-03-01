3e655b99cca5d9d7b8b9e911fa7f83b1c775a62c
Atanas Ivanov <nasko.1806@gmail.com>
2024-03-02 00:58:38 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'web-prism-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'web-prism';
+  title = 'Web prism';
 }
