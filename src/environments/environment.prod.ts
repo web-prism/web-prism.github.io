13ceb0ed4f3dcd77938ed1fb43982c7224222f72
Atanas Ivanov <nasko.1806@gmail.com>
2024-03-18 23:46:55 +0200

    env fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,8 @@ export const environment = {
     messagingSenderId: '231344390085',
     appId: '1:231344390085:web:3de358e99c13dc287095ac',
     measurementId: 'G-1G5775G7HJ',
-    apiUrlGlitch:
+    apiUrl:
       'https://web-prism-4d19e-default-rtdb.europe-west1.firebasedatabase.app',
+    apiUrlGlitch: 'https://web-prism.glitch.me/api',
   },
 };
